0eac027757f310e413208af4bfd9c8510e555519
ohyoungjooung2 <wnapdlf05@gmail.com>
2017-07-23 22:11:34 +0900

    just testing git

--- stable.sh
+++ stable.sh
@@ -6,5 +6,5 @@ TARGET="for-ruby-stable.html"
 wget https://www.ruby-lang.org/en/downloads/ -O $TARGET -q
 CS=$(grep -i "current stable" $TARGET | head -1 | awk '{print $6}' | sed 's/\.$//g')
 }
-
+#Execute
 get_stable
